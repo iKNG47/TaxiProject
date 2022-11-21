f174ede527f299ccdc02fd5639472f39ac4fb1b2
Igor <igorko098@gmail.com>
2022-11-21 23:21:08 +0100

    created Manufacturer model and dao

--- src/main/java/taxi/project/Main.java
+++ src/main/java/taxi/project/Main.java
@@ -1,13 +1,14 @@
 package taxi.project;
 
 import taxi.project.dao.DriverDao;
+import taxi.project.dao.ManufacturerDao;
 import taxi.project.dao.impl.DriverDaoImpl;
+import taxi.project.dao.impl.ManufacturerDaoImpl;
 import taxi.project.model.Driver;
+import taxi.project.model.Manufacturer;
 
 public class Main {
     public static void main(String[] args) {
-        DriverDao dao = new DriverDaoImpl();
-        Driver driver = new Driver("igor","login","asd","154");
-        dao.create(driver);
+
     }
 }
