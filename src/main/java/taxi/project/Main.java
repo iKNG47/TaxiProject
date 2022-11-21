cd27184c588f4d393f4b6f49e2b801f843a9c19e
Igor <igorko098@gmail.com>
2022-11-21 23:31:30 +0100

    created Car model and dao

--- src/main/java/taxi/project/Main.java
+++ src/main/java/taxi/project/Main.java
@@ -1,12 +1,5 @@
 package taxi.project;
 
-import taxi.project.dao.DriverDao;
-import taxi.project.dao.ManufacturerDao;
-import taxi.project.dao.impl.DriverDaoImpl;
-import taxi.project.dao.impl.ManufacturerDaoImpl;
-import taxi.project.model.Driver;
-import taxi.project.model.Manufacturer;
-
 public class Main {
     public static void main(String[] args) {
 
